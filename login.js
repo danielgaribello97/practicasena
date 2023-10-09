3333e247fd853262c2d9fdbf4fa331ce2d199ddb
danielgaribello97 <145881355+danielgaribello97@users.noreply.github.com>
2023-10-09 09:58:48 -0500

    Update login.js

--- login.js
+++ login.js
@@ -9,3 +9,15 @@ function verificarCredenciales() {
         alert("Usuario y/o contraseña incorrectos. Intenta de nuevo.");
     }
 }
+
+function registrarUsuario() {
+    var nuevoUsuario = prompt("Ingresa un nombre de usuario:");
+    var nuevaContrasena = prompt("Ingresa una contraseña:");
+
+    if (nuevoUsuario && nuevaContrasena) {
+        // Aquí puedes agregar código para guardar el nuevo usuario y contraseña
+        alert("Usuario registrado exitosamente. Puedes iniciar sesión ahora.");
+    } else {
+        alert("No se pudo registrar el usuario. Asegúrate de proporcionar un nombre de usuario y una contraseña.");
+    }
+}
