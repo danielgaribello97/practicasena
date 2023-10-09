19ee7c6b98c1dca301f7431dd5e9e5e0676e936b
danielgaribello97 <145881355+danielgaribello97@users.noreply.github.com>
2023-10-09 11:05:19 -0500

    Update login.js

--- login.js
+++ login.js
@@ -36,3 +36,34 @@ function registrarUsuario() {
         alert("No se pudo registrar el usuario. Asegúrate de proporcionar un nombre de usuario y una contraseña.");
     }
 }
+
+function eliminarUsuario() {
+    var usuarioAEliminar = prompt("Ingresa el nombre de usuario que deseas eliminar:");
+
+    if (usuarioAEliminar) {
+        var usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+        var usuariosFiltrados = usuariosRegistrados.filter(function(user) {
+            return user.usuario !== usuarioAEliminar;
+        });
+
+        if (usuariosFiltrados.length < usuariosRegistrados.length) {
+            localStorage.setItem('usuarios', JSON.stringify(usuariosFiltrados));
+            alert("Usuario eliminado exitosamente.");
+        } else {
+            alert("No se encontró el usuario especificado.");
+        }
+    } else {
+        alert("No se proporcionó un nombre de usuario.");
+    }
+}
+
+function verificarContrasenaAdmin() {
+    var contrasenaAdmin = document.getElementById("contrasena_admin").value;
+
+    if (contrasenaAdmin === "D8n456to") {
+        eliminarUsuario();
+    } else {
+        alert("Contraseña de administrador incorrecta. No puedes eliminar usuarios.");
+    }
+}
