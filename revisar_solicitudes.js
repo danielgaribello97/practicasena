ec9feb88addcb2ee5cb75dd56d1d009c43ca251c
danielgaribello97 <145881355+danielgaribello97@users.noreply.github.com>
2023-10-10 19:23:22 -0500

    Update revisar_solicitudes.js

--- revisar_solicitudes.js
+++ revisar_solicitudes.js
@@ -1,24 +1,29 @@
-function verificarCredenciales() {
-    var usuario = document.getElementById("usuario").value;
-    var contrasena = document.getElementById("contrasena").value;
-
-    if (usuario === "Daniel" && contrasena === "S510n45to") {
-        cargarSolicitudesPendientes();
-    } else {
-        alert("Usuario y/o contraseña incorrectos. Intenta de nuevo.");
-    }
-}
-
-function cargarSolicitudesPendientes() {
+unction cargarSolicitudesPendientes() {
     var solicitudesPendientes = JSON.parse(localStorage.getItem('solicitudes')) || [];
     var listaSolicitudes = document.getElementById('solicitudes-list');
 
     if (solicitudesPendientes.length > 0) {
         listaSolicitudes.innerHTML = ""; // Limpiar la lista antes de agregar elementos
 
-        solicitudesPendientes.forEach(function(solicitud) {
+        solicitudesPendientes.forEach(function(solicitud, index) {
             var itemSolicitud = document.createElement('li');
-            itemSolicitud.textContent = 'Usuario: ' + solicitud.usuario + ', Contraseña: ' + solicitud.contrasena;
+            itemSolicitud.textContent = 'Solicitud ' + (index + 1) + ': Usuario: ' + solicitud.usuario + ', Contraseña: ' + solicitud.contrasena;
+
+            var aprobarBtn = document.createElement('button');
+            aprobarBtn.textContent = 'Aprobar';
+            aprobarBtn.onclick = function() {
+                aprobarSolicitud(index);
+            };
+
+            var rechazarBtn = document.createElement('button');
+            rechazarBtn.textContent = 'Rechazar';
+            rechazarBtn.onclick = function() {
+                rechazarSolicitud(index);
+            };
+
+            itemSolicitud.appendChild(aprobarBtn);
+            itemSolicitud.appendChild(rechazarBtn);
+
             listaSolicitudes.appendChild(itemSolicitud);
         });
     } else {
@@ -27,3 +32,29 @@ function cargarSolicitudesPendientes() {
         listaSolicitudes.appendChild(mensaje);
     }
 }
+
+function aprobarSolicitud(index) {
+    var solicitudesPendientes = JSON.parse(localStorage.getItem('solicitudes')) || [];
+    var nuevoUsuario = solicitudesPendientes[index];
+
+    var usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios')) || [];
+    usuariosRegistrados.push(nuevoUsuario);
+
+    localStorage.setItem('usuarios', JSON.stringify(usuariosRegistrados));
+
+    solicitudesPendientes.splice(index, 1);
+    localStorage.setItem('solicitudes', JSON.stringify(solicitudesPendientes));
+
+    cargarSolicitudesPendientes();
+}
+
+function rechazarSolicitud(index) {
+    var solicitudesPendientes = JSON.parse(localStorage.getItem('solicitudes')) || [];
+    solicitudesPendientes.splice(index, 1);
+    localStorage.setItem('solicitudes', JSON.stringify(solicitudesPendientes));
+
+    cargarSolicitudesPendientes();
+}
+
+// Asegúrate de cargar las solicitudes pendientes al cargar la página.
+window.onload = cargarSolicitudesPendientes;
