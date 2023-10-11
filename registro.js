5bc1862a411532cc1d6d61daf9e0f4c8fade75b0
danielgaribello97 <145881355+danielgaribello97@users.noreply.github.com>
2023-10-10 20:06:13 -0500

    Create registro.js

---
+++ registro.js
@@ -0,0 +1,20 @@
+function registrarNuevoUsuario() {
+    var nuevoUsuario = document.getElementById("usuario_registro").value;
+    var nuevaContrasena = document.getElementById("contrasena_registro").value;
+
+    if (nuevoUsuario && nuevaContrasena) {
+        var usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+        var nuevoUsuarioObj = {
+            usuario: nuevoUsuario,
+            contrasena: nuevaContrasena
+        };
+
+        usuariosRegistrados.push(nuevoUsuarioObj);
+        localStorage.setItem('usuarios', JSON.stringify(usuariosRegistrados));
+
+        alert("Usuario registrado exitosamente. Puedes iniciar sesión ahora.");
+    } else {
+        alert("No se pudo registrar el usuario. Asegúrate de proporcionar un nombre de usuario y una contraseña.");
+    }
+}
