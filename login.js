2167a6703f123f78d50309849c8262be604046fd
danielgaribello97 <145881355+danielgaribello97@users.noreply.github.com>
2023-10-10 19:30:57 -0500

    Update login.js

--- login.js
+++ login.js
@@ -9,61 +9,27 @@ function verificarCredenciales() {
     });
 
     if (usuarioValido) {
-        alert("¡Ingreso exitoso!");
-        window.location.replace("pagina_secreta.html");
+        var codigoVerificacion = generarCodigoVerificacion();
+        enviarCodigoAlCorreo(codigoVerificacion); // Esta función enviará el código al correo del administrador.
+
+        var codigoIngresado = prompt("Se ha enviado un código de verificación al correo del administrador. Ingresa el código:");
+
+        if (codigoIngresado === codigoVerificacion) {
+            alert("¡Ingreso exitoso!");
+            window.location.replace("pagina_secreta.html");
+        } else {
+            alert("Código de verificación incorrecto. Intenta de nuevo.");
+        }
     } else {
         alert("Usuario y/o contraseña incorrectos. Intenta de nuevo.");
     }
 }
 
-function registrarUsuario() {
-    var nuevoUsuario = prompt("Ingresa un nombre de usuario:");
-    var nuevaContrasena = prompt("Ingresa una contraseña:");
-
-    if (nuevoUsuario && nuevaContrasena) {
-        var usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios')) || [];
-
-        var nuevoUsuarioObj = {
-            usuario: nuevoUsuario,
-            contrasena: nuevaContrasena
-        };
-
-        usuariosRegistrados.push(nuevoUsuarioObj);
-        localStorage.setItem('usuarios', JSON.stringify(usuariosRegistrados));
-
-        alert("Usuario registrado exitosamente. Puedes iniciar sesión ahora.");
-    } else {
-        alert("No se pudo registrar el usuario. Asegúrate de proporcionar un nombre de usuario y una contraseña.");
-    }
+function generarCodigoVerificacion() {
+    return Math.floor(100000 + Math.random() * 900000); // Genera un código de 6 dígitos.
 }
 
-function eliminarUsuario() {
-    var usuarioAEliminar = prompt("Ingresa el nombre de usuario que deseas eliminar:");
-
-    if (usuarioAEliminar) {
-        var usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios')) || [];
-
-        var usuariosFiltrados = usuariosRegistrados.filter(function(user) {
-            return user.usuario !== usuarioAEliminar;
-        });
-
-        if (usuariosFiltrados.length < usuariosRegistrados.length) {
-            localStorage.setItem('usuarios', JSON.stringify(usuariosFiltrados));
-            alert("Usuario eliminado exitosamente.");
-        } else {
-            alert("No se encontró el usuario especificado.");
-        }
-    } else {
-        alert("No se proporcionó un nombre de usuario.");
-    }
-}
-
-function verificarContrasenaAdmin() {
-    var contrasenaAdmin = document.getElementById("contrasena_admin").value;
-
-    if (contrasenaAdmin === "D8n456to") {
-        eliminarUsuario();
-    } else {
-        alert("Contraseña de administrador incorrecta. No puedes eliminar usuarios.");
-    }
+function enviarCodigoAlCorreo(codigo) {
+    // Aquí se simula el envío de un correo electrónico al administrador.
+    console.log("Se ha enviado el código de verificación al correo del administrador: " + codigo);
 }
