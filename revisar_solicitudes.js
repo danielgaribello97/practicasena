a6bcd95fd117c2dc3cc5b05d7b00d5e082d4fa3a
danielgaribello97 <145881355+danielgaribello97@users.noreply.github.com>
2023-10-10 19:11:22 -0500

    Update revisar_solicitudes.js

--- revisar_solicitudes.js
+++ revisar_solicitudes.js
@@ -13,4 +13,17 @@ function cargarSolicitudesPendientes() {
     var solicitudesPendientes = JSON.parse(localStorage.getItem('solicitudes')) || [];
     var listaSolicitudes = document.getElementById('solicitudes-list');
 
-    if (solicitudesPendientes.length > 0
+    if (solicitudesPendientes.length > 0) {
+        listaSolicitudes.innerHTML = ""; // Limpiar la lista antes de agregar elementos
+
+        solicitudesPendientes.forEach(function(solicitud) {
+            var itemSolicitud = document.createElement('li');
+            itemSolicitud.textContent = 'Usuario: ' + solicitud.usuario + ', Contraseña: ' + solicitud.contrasena;
+            listaSolicitudes.appendChild(itemSolicitud);
+        });
+    } else {
+        var mensaje = document.createElement('p');
+        mensaje.textContent = 'No hay solicitudes pendientes.';
+        listaSolicitudes.appendChild(mensaje);
+    }
+}
