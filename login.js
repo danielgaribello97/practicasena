b1698150386034543afaaa442934cdb34f5584d1
danielgaribello97 <145881355+danielgaribello97@users.noreply.github.com>
2023-10-10 20:57:24 -0500

    Update login.js

--- login.js
+++ login.js
@@ -11,7 +11,20 @@ function verificarCredenciales() {
     if (usuarioValido) {
         var codigoIngresado = prompt("Se ha enviado un código de verificación al correo del administrador. Ingresa el código:");
 
-        if (codigoIngresado === "6473,6501") {
+        if (codigoIngresado === "6473") {
+            alert("¡Ingreso exitoso!");
+            window.location.replace("pagina_secreta.html");
+        } else {
+            alert("Código de verificación incorrecto. Intenta de nuevo.");
+        }
+    } else {
+        alert("Usuario y/o contraseña incorrectos. Intenta de nuevo.");
+    }
+}
+    if (usuarioValido) {
+        var codigoIngresado = prompt("Se ha enviado un código de verificación al correo del administrador. Ingresa el código:");
+
+        if (codigoIngresado === "6501") {
             alert("¡Ingreso exitoso!");
             window.location.replace("pagina_secreta.html");
         } else {
