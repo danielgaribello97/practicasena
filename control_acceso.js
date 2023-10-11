fdf7fb53c693ec960e6a18ee3646b454112636bf
danielgaribello97 <145881355+danielgaribello97@users.noreply.github.com>
2023-10-10 20:54:16 -0500

    Update control_acceso.js

--- control_acceso.js
+++ control_acceso.js
@@ -1,8 +1,12 @@
 function verificarAcceso() {
-    var usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios')) || [];
+    var codigoIngresado = prompt("Se ha enviado un código de verificación al correo del administrador. Ingresa el código:");
 
-    if (usuariosRegistrados.length === 0) {
-        alert("No hay usuarios registrados. Por favor, regístrate primero.");
+    var codigosPermitidos = ["6473", "6501", "0001", "0002", "5566", "9875", "4787"];
+
+    if (codigosPermitidos.includes(codigoIngresado)) {
+        alert("Código correcto. Acceso permitido.");
         window.location.replace("login.html");
+    } else {
+        alert("Código incorrecto. Acceso denegado.");
     }
 }
